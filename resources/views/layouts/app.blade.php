6cd5c5f3620fc2b31365f4fb54392fb9a2c56769
SebastianoMV <sebastianovisco@gmail.com>
2022-07-05 16:05:28 +0200

    Nascosto link Posts se non loggato

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,9 +26,12 @@
                 <a class="navbar-brand" href="{{ route('home') }}">
                     Vai alla pagina pubblica
                 </a>
-                <a class="navbar-brand" href="{{ route('admin.posts.index') }}">
-                    Posts
-                </a>
+                @auth
+                    <a class="navbar-brand" href="{{ route('admin.posts.index') }}">
+                        Posts
+                    </a>  
+                @endauth
+                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
